002d3c007d4018aca0069a65931e2d2d1e7b67e3
Nguyễn Nguyên Hảo <147150614+haourgot123@users.noreply.github.com>
2024-09-28 21:57:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,5 @@ st.info('This is my Machine Learning App')
 
 with st.expander('Data'):
   st.write('**Raw data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df
