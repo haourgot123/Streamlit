26c8610c677679b8697a44df4cb5a451104e6908
Nguyễn Nguyên Hảo <147150614+haourgot123@users.noreply.github.com>
2024-09-28 22:02:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,5 +18,5 @@ with st.expander('Data'):
 with st.expander('Data visualization'):
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
 
-with st.slidebar:
+with st.sidebar:
   st.header('Input features')
