2a7d6cc7321538a6125117adfdb03d68c8ee5127
Nguyễn Nguyên Hảo <147150614+haourgot123@users.noreply.github.com>
2024-09-28 21:59:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,3 +9,10 @@ with st.expander('Data'):
   st.write('**Raw data**')
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
   df
+  st.write('**x**')
+  X_raw = df.drop('species', axis = 1)
+  X_raw
+  st.write('**y**')
+  y_raw = df.species
+  y_raw
+  
