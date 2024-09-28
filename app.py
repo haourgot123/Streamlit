b694a68aaa921b69d53de612f6456226e278600f
Nguyễn Nguyên Hảo <147150614+haourgot123@users.noreply.github.com>
2024-09-28 22:11:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -35,3 +35,8 @@ with st.sidebar:
           'sex': gender}
   input_df = pd.DataFrame(data, index=[0])
   input_penguins = pd.concat([input_df, X_raw], axis=0)
+with st.expander('Input features'):
+  st.write('**Input penguin**')
+  input_df
+  st.write('**Combined penguins data**')
+  input_penguins
