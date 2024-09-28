14e991289d6b85c61f4d9c5b844c3e5106d9f092
Nguyễn Nguyên Hảo <147150614+haourgot123@users.noreply.github.com>
2024-09-28 22:04:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -21,3 +21,5 @@ with st.expander('Data visualization'):
 with st.sidebar:
   st.header('Input features')
   island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgersen'))
+  bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
+  bill_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2)
